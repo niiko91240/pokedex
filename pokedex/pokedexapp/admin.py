8698ab705655dd3ed6312b7bdfdf8c4b26c7ccc3
niiko fx <nicolas.ajrd@gmail.com>
2022-11-15 11:31:29 +0100

    Suppr django filters

--- pokedex/pokedexapp/admin.py
+++ pokedex/pokedexapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Pokemon, Type
+from .models import Pokemon, Type, Equipe
 
 # Register your models here.
 admin.site.register(Pokemon)
 admin.site.register(Type)
+admin.site.register(Equipe)
