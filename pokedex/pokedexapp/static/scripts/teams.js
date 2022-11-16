b0f8a4ecfdfedb792de2b27ea9d2fb3f05241afd
niiko fx <nicolas.ajrd@gmail.com>
2022-11-16 11:16:13 +0100

    Selection de pokemon pour les équipes

---
+++ pokedex/pokedexapp/static/scripts/teams.js
@@ -0,0 +1,21 @@
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
+var grid = document.getElementById('container2');
+
+function afficherPokemon(){
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+function createTeam(){
+  grid.innerHTML += '<div class="case"><p>Equipe 1</p><div class="circle" id="add-pokemon" onclick="afficherPokemon();"><img src="/static/images/plus-solid.svg"></div><div class="circle" id="add-pokemon" onclick="afficherPokemon();"><img src="/static/images/plus-solid.svg"></div><div class="circle" id="add-pokemon" onclick="afficherPokemon();"><img src="/static/images/plus-solid.svg"></div><div class="circle" id="add-pokemon" onclick="afficherPokemon();"><img src="/static/images/plus-solid.svg"></div><div class="circle" id="add-pokemon" onclick="afficherPokemon();"><img src="/static/images/plus-solid.svg">></div><img class="trash" src="/static/images/trash-can-regular.svg"></div>'
+}
